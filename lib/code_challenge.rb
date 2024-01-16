6b8e429bec58ee930c7d17e0d12bd13b4721e732
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-16 14:21:19 -0600

    added coding challenges for ruby and javascript as well

---
+++ lib/code_challenge.rb
@@ -0,0 +1,39 @@
+## 1: define function that takes in a single integer, and if it is even, return True, else return false
+
+
+
+
+def is_even(number)
+    number % 2 == 0
+end
+
+puts "2 is even? #{is_even(2)}"
+puts "5 is even? #{is_even(5)}"
+
+
+
+
+########################
+
+
+
+
+## 2: define function that takes in a list, and returns the sum of all the numbers
+
+
+
+def sum_of_list(numbers)
+    numbers.sum
+end
+
+
+puts "sum of list: #{sum_of_list([1, 2, 3, 4, 5])}"
+
+
+
+
+########################
+
+
+
+## 3: 
