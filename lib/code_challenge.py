e48a88d25e5b4a26486682d18fb964079174086b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-16 21:47:13 -0600

    added more code challenges to Python and JavaScript files

--- lib/code_challenge.py
+++ lib/code_challenge.py
@@ -99,36 +99,94 @@ print("\n#######\n")
 
 
 
-## 6: define a function that 
+## 6: define a function that takes in an integer and returns True or False checking whether it is a Prime Number
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+print("11 is prime? -->", is_prime(11))
+print("4 is prime? -->", is_prime(4))
+
+
+print("\n#######\n")
 
 
 
-# print("\n#######\n")
+## 7: define a function that finds the longest word in a sentence
+
+
+def longest_word(sentence):
+    words = sentence.split()
+    max_word = words[0]
+    for word in words:
+        if len(word) > len(max_word):
+            max_word = word
+    return max_word
+
+sentence = "cool cats are cool but I am the coolest"
+print("what's the longest word in the sentence? -->", longest_word(sentence))
 
 
 
-## 7: define a function that 
+print("\n#######\n")
 
 
 
-# print("\n#######\n")
+## 8: define a function that takes a list of numbers and returns a new list containing only the square of the even numbers from the original list
+
+
+def square_of_even(numbers):
+    return [x**2 for x in numbers if x % 2 == 0]
+
+print("return the squares of only the even numbers of this list [1, 2, 3, 4, 5]: -->", square_of_even([1, 2, 3, 4, 5]))
 
 
 
-## 8: define a function that 
+print("\n#######\n")
 
 
 
-# print("\n#######\n")
+## 9: define a function that calculates the factorial of a number
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+print("what is the factorial of 4? -->", factorial(4))
 
 
 
-## 9: define a function that 
+print("\n#######\n")
 
 
 
-# print("\n#######\n")
+## 10: define a function that takes in two dictionaries and merges them. However, for keys that exist in both dictionaries, only add the key-value pair to the resulting dictionary if the value associated with the key is greater in the second dictionary
 
 
 
-## 10: define a function that 
+def merge_dictionaries(dict1, dict2):
+    merged_dict = dict1.copy()
+    for key, value in dict2.items():
+        if key not in dict1 or value > dict1[key]:
+            merged_dict[key] = value
+    return merged_dict
+
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"b": 3, "c": 2, "d": 4}
+
+print(merge_dictionaries(dict1, dict2))
+
+
+
+print("\n#######\n")
