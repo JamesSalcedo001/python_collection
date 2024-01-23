1b6b9d8970618bd75c9ab34ca0dd272ca3aca73c
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-23 17:35:14 -0600

    starting over

--- lib/code_challenge.py
+++ lib/code_challenge.py
@@ -1,192 +1,35 @@
-## 1: define function that takes in a single integer, and if it is even, return True, else return false
+# Arrays
 
 
-def is_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-    
 
-print("2 is even? -->", (is_even(2)))
-print("5 is even? -->", (is_even(5)))
+# 1: Two Sum
 
 
 
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-print("\n###########\n")
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+# You can return the answer in any order.
 
 
 
-## 2: define function that takes in a list, and returns the sum of all the numbers
 
 
+# 2:
 
-def sum_of_list(numbers):
-    total = 0
+# 3:
 
-    for num in numbers:
-        total += num
-    return total
+# 4:
 
+# 5:
 
-print("sum of list: [1, 2, 3, 4, 5] -->", sum_of_list([1, 2, 3, 4, 5]))
+# 6:
 
+# 7:
 
+# 8:
 
+# 9:
 
-print("\n###########\n")
-
-
-
-## 3: define function that takes in a list of integers, and returns the count of even integers in the list
-
-
-
-def count_even_numbers(numbers):
-    count = 0
-    for number in numbers:
-        if is_even(number):
-            count += 1
-    return count
-
-print("how many even numbers? [1, 2, 3, 4, 5] -->", (count_even_numbers([1, 2, 3, 4, 5])))
-
-
-
-print("\n###########\n")
-
-
-
-## 4: define a function that takes in a string, reverses it, and returns the reversed string
-
-
-
-
-def reverse_string(s):
-    reversed_string = ""
-
-    for char in s:
-        reversed_string = char + reversed_string
-    return reversed_string
-
-print("reverse the string 'hello' -->", (reverse_string("hello")))
-
-
-
-print("\n###########\n")
-
-
-
-## 5: define a function that takes in a list of integers and returns the maximum
-
-
-
-def find_max(numbers):
-    max_number = numbers[0]
-
-    for number in numbers:
-        if number > max_number:
-            max_number = number
-    return max_number
-
-print("find the maximum number in array [1, 2, 3, 4, 5] -->", find_max([1, 2, 3, 4, 5]))
-
-
-
-print("\n#######\n")
-
-
-
-## 6: define a function that takes in an integer and returns True or False checking whether it is a Prime Number
-
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
-
-
-print("11 is prime? -->", is_prime(11))
-print("4 is prime? -->", is_prime(4))
-
-
-print("\n#######\n")
-
-
-
-## 7: define a function that finds the longest word in a sentence
-
-
-def longest_word(sentence):
-    words = sentence.split()
-    max_word = words[0]
-    for word in words:
-        if len(word) > len(max_word):
-            max_word = word
-    return max_word
-
-sentence = "cool cats are cool but I am the coolest"
-print("what's the longest word in the sentence? -->", longest_word(sentence))
-
-
-
-print("\n#######\n")
-
-
-
-## 8: define a function that takes a list of numbers and returns a new list containing only the square of the even numbers from the original list
-
-
-def square_of_even(numbers):
-    return [x**2 for x in numbers if x % 2 == 0]
-
-print("return the squares of only the even numbers of this list [1, 2, 3, 4, 5]: -->", square_of_even([1, 2, 3, 4, 5]))
-
-
-
-print("\n#######\n")
-
-
-
-## 9: define a function that calculates the factorial of a number
-
-
-def factorial(n):
-    if n == 0:
-        return 1
-    result = 1
-    for i in range(2, n + 1):
-        result *= i
-    return result
-
-print("what is the factorial of 4? -->", factorial(4))
-
-
-
-print("\n#######\n")
-
-
-
-## 10: define a function that takes in two dictionaries and merges them. However, for keys that exist in both dictionaries, only add the key-value pair to the resulting dictionary if the value associated with the key is greater in the second dictionary
-
-
-
-def merge_dictionaries(dict1, dict2):
-    merged_dict = dict1.copy()
-    for key, value in dict2.items():
-        if key not in dict1 or value > dict1[key]:
-            merged_dict[key] = value
-    return merged_dict
-
-dict1 = {"a": 1, "b": 2, "c": 3}
-dict2 = {"b": 3, "c": 2, "d": 4}
-
-print(merge_dictionaries(dict1, dict2))
-
-
-
-print("\n#######\n")
+# 10:
--- lib/code_challenge.rb
+++ lib/code_challenge.rb
@@ -1,173 +1,19 @@
-require "colorize"
+# 1:
 
-## 1: define function that takes in a single integer, and if it is even, return True, else return false
+# 2:
 
+# 3:
 
+# 4:
 
+# 5:
 
-def is_even(number)
-    number % 2 == 0
-end
+# 6:
 
-puts "2 is even? -->".blue.bold.underline + " #{is_even(2)}".red
-puts "5 is even? -->".blue.bold.underline + " #{is_even(5)}".red
+# 7:
 
+# 8:
 
+# 9:
 
-
-puts "\n#######\n".green
-
-
-
-
-## 2: define function that takes in a list, and returns the sum of all the numbers
-
-
-
-def sum_of_list(numbers)
-    numbers.sum
-end
-
-
-puts "sum of list: -->".blue.bold.underline + " #{sum_of_list([1, 2, 3, 4, 5])}".red
-
-
-
-
-puts "\n#######\n".green
-
-
-
-
-## 3: define function that takes in a list of integers, and returns the count of even integers in the list
-
-
-
-def count_even_numbers(numbers)
-    numbers.count { |number| number.even? }
-end
-
-puts "How many even numbers? --->".blue.bold.underline + " #{count_even_numbers([1, 2, 3, 4, 5])}".red
-
-puts "\n#######\n".green
-
-
-
-## 4: define a function that takes in a string, reverses it, and returns the reversed string
-
-
-
-
-def reverse_string(s)
-    s.reverse
-end
-
-puts "Reverse the string -->".blue.bold.underline + " #{reverse_string("hello")}".red
-
-
-puts "\n#######\n".green
-
-
-
-## 5: define a function that takes in a list of integers and returns the maximum
-
-
-
-def find_max(numbers)
-    numbers.max
-end
-
-puts "Find the max number in array -->".blue.bold.underline + " #{find_max([1, 2, 3, 4, 5])}".red
-
-
-
-puts "\n#######\n".green
-
-
-
-
-## 6: define a function that takes in an integer and returns True or False checking whether it is a Prime Number
-
-def is_prime(number)
-    return false if number <= 1
-    (2..Math.sqrt(number)).each do |i|
-        return false if number % i == 0
-    end
-    true
-end
-
-
-puts "11 is prime? -->".blue.bold.underline + " #{is_prime(11)}"
-puts "4 is prime? -->".blue.bold.underline + " #{is_prime(4)}"
-
-
-puts "\n#######\n".green
-
-
-
-## 7: define a function that finds the longest word in a sentence
-
-
-def longest_word(sentence)
-    sentence.split.max_by(&:length)
-end
-
-sentence = "cool cats are cool but I am the coolest"
-puts "what's the longest word in the sentence? -->".blue.bold.underline + " #{ longest_word(sentence)}"
-
-
-
-puts "\n#######\n".green
-
-
-
-## 8: define a function that takes a list of numbers and returns a new list containing only the square of the even numbers from the original list
-
-
-def square_of_even(numbers)
-    numbers.select { |x| x.even? }.map { |x| x**2 }
-end
-
-puts "return the squares of only the even numbers of this list [1, 2, 3, 4, 5]: -->".blue.bold.underline + " #{square_of_even([1, 2, 3, 4, 5])}"
-
-
-
-puts "\n#######\n".green
-
-
-
-## 9: define a function that calculates the factorial of a number
-
-
-def factorial(n)
-    return 1 if n == 0
-    (1..n).inject(:*)
-end
-
-puts "what is the factorial of 4? -->".blue.bold.underline + " #{factorial(4)}"
-
-
-
-puts "\n#######\n".green
-
-
-
-## 10: define a function that takes in two hashes and merges them. However, for keys that exist in both hashes, only add the key-value pair to the resulting hash if the value associated with the key is greater in the second hash
-
-
-
-def merge_hashes(hash1, hash2)
-    hash2.each do |key, value|
-        hash1[key] = value if !hash1.key?(key) || value > hash1[key]
-    end
-    hash1
-end
-
-hash1 = {"a" => 1, "b" => 2, "c" => 3}
-hash2 = {"b" => 3, "c" => 2, "d" => 4}
-
-puts "two hashes merged -->".blue.bold.underline + " #{merge_hashes(hash1, hash2)}"
-
-
-
-puts "\n#######\n".green
+# 10:
