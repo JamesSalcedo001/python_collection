d4c28097959cd637c71517202ff016ab4b82c304
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-02-08 10:12:35 -0600

    lala

--- lib/code_challenge.py
+++ lib/code_challenge.py
@@ -13,8 +13,15 @@
 # You can return the answer in any order.
 
 
+def twoSum(nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+            return []
 
 
+print(twoSum([1, 2, 3], 4))
 
 # 2:
 
--- lib/code_challenge.rb
+++ lib/code_challenge.rb
@@ -1,4 +1,29 @@
-# 1:
+# Arrays
+
+
+
+# 1: Two Sum
+
+
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+
+def two_sum(nums, target)
+    nums.each_with_index do |num1, i|
+        nums.each_with_index do |num2, j|
+            return [i, j] if i != j && num1 + num2 == target
+        end
+    end
+    []
+end
+
+
+puts two_sum([3, 3], 6)
 
 # 2:
 
